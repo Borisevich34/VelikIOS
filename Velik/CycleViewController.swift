bf30f2d3f7d9c860f49b6cd922f476c7dd9bfe36
Pavel Borisevich <borisevich_pavel@bk.ru>
2017-04-05 15:44:20 +0300

    Beautiful pages are added

--- Velik/CycleViewController.swift
+++ Velik/CycleViewController.swift
@@ -12,26 +12,27 @@ class CycleViewController: UIViewController {
 
     weak var cycle: Cycle?
     @IBOutlet weak var makeOrderButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pagesControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MARK - number of pages depends from Backendless side
+        //pagesControl.numberOfPages = 3
+        //pagesControl.currentPage = 0
         makeOrderButton.layer.cornerRadius = 12.0
+        scrollView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension CycleViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pagesControl.currentPage = Int(scrollView.contentOffset.x/scrollView.frame.width)
     }
-    */
-
 }
